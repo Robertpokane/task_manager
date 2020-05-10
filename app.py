579243f03b4b057cb0c2428ed4ebd4a6745a7bde
Ubuntu <ubuntu@ip-172-31-83-91.ec2.internal>
2020-05-10 16:34:05 +0000

    added full form for adding a task, task name, description, duedate callender selector, if it is urgent. added backend functionality to add task to mongodb tasks database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -17,8 +17,15 @@ def get_tasks():
     
 @app.route('/add_task')
 def add_task():
-    return render_template("addtask.html")
-    
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=["POST"])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
